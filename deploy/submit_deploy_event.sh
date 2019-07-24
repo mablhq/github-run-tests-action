4b1aa5ab503c60018b8ce7637de2f027791f213d
jif <geoff@mabl.com>
2019-07-24 17:36:22 -0400

    add support for polling for results to mabl deployment action

--- deploy/submit_deploy_event.sh
+++ deploy/submit_deploy_event.sh
@@ -42,7 +42,55 @@ if ! [ "${status_code}" == "null" ]; then
     exit 20
 else
     echo "Deployment event notification successful"
-    mkdir "${HOME}/mabl"
-    echo "${DEPLOYMENT_EVENT_ID}" > "${HOME}/mabl/deployment_event_id"
 fi
+
+echo "Checking for results"
+
+COMPLETION_WAIT_MAX_WAIT_MILLISECONDS=$((30*60*1000))
+COMPLETION_WAIT_POOLING_SECONDS=15
+
+COMPLETION_WAIT_MAX_TIME_MILLISECONDS=$((`date +%s%3N`+${COMPLETION_WAIT_MAX_WAIT_MILLISECONDS})) # 30 minutes
+WAIT_FOR_MABL_TEST_SUCCESS=1
+EXECUTION_RESULTS_URL="https://${!MABL_API_HOST}/execution/result/event/${DEPLOYMENT_EVENT_ID}";
+
+EXECUTION_RESULTS_JSON_PATH=$(mktemp)
+
+while true; do
+
+    # don't fail build on API error
+    set +e
+    curl -s ${EXECUTION_RESULTS_URL} \
+    -u "key:${!MABL_API_KEY}" \
+    -H "Content-Type:application/json" > ${EXECUTION_RESULTS_JSON_PATH}
+    set -e
+
+    set +e
+    EXECUTION_RESULTS_ALL_SUCCESS_FALSE_COUNT=$(cat ${EXECUTION_RESULTS_JSON_PATH} | jq '.executions[].success' -r | grep -c 'false')
+    EXECUTION_RESULTS_STATUS_COMPLETE_COUNT=$(cat ${EXECUTION_RESULTS_JSON_PATH} | jq '.executions[].status' -r | egrep -c '(succeeded|failed|cancelled|completed)')
+    EXECUTION_RESULTS_STATUS_COUNT=$(cat ${EXECUTION_RESULTS_JSON_PATH} | jq '.executions[].success' -r | wc -l)
+    set -e
+
+    if [[ ${EXECUTION_RESULTS_STATUS_COMPLETE_COUNT} -eq ${EXECUTION_RESULTS_STATUS_COUNT} ]]; then
+      if [[ ${EXECUTION_RESULTS_ALL_SUCCESS_FALSE_COUNT} -gt 0 ]]; then
+        echo "Completed running ${EXECUTION_RESULTS_STATUS_COUNT} tests, with ${EXECUTION_RESULTS_ALL_SUCCESS_FALSE_COUNT} failures."
+        exit 21
+      fi
+
+      echo "Completed running ${EXECUTION_RESULTS_STATUS_COUNT} tests.  All tests PASSED!"
+      exit 0
+    fi
+
+    echo "Current completion rate: [${EXECUTION_RESULTS_STATUS_COMPLETE_COUNT}/${EXECUTION_RESULTS_STATUS_COUNT}]"
+
+    # Have we hit max timeout?
+    CURRENT_TIME_MILLISECONDS=`date +%s%3N`
+
+    if [[ ${CURRENT_TIME_MILLISECONDS} -gt ${COMPLETION_WAIT_MAX_TIME_MILLISECONDS} ]]; then
+      echo "FAILED! Max timeout exceeded! Aborting waiting for tests!"
+      exit 22
+    fi
+
+    # Sleep and try again
+    sleep ${COMPLETION_WAIT_POOLING_SECONDS}
+done
 
