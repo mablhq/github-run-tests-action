b7d9336be418b08db7e256ce9de49847e219b0d8
jif <geoff@mabl.com>
2019-07-25 16:31:32 -0400

    adding node modules into the commit as required

---
+++ deploy-and-test/lib/tests/_suite.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+const assert = __importStar(require("assert"));
+const mablApiClient_1 = require("../mablApiClient");
+describe('azure pipeline task tests', function () {
+    before(function () { });
+    after(() => { });
+    it('builds the request correctly with all options', function (done) {
+        let expected = '{"environment_id":"env","application_id":"app","plan_overrides":{"browser_types":["firefox"," chrome"," internet_explorer"],"uri":"uri"},"actions":{"rebaseline_images":true,"set_static_baseline":true}}';
+        let apiClient = new mablApiClient_1.mablApiClient('test');
+        let requestBody = apiClient.buildRequestBody('app', 'env', 'firefox, chrome, internet_explorer', 'uri', true, true);
+        assert.equal(expected, JSON.stringify(requestBody));
+        done();
+    });
+    it('builds the request correctly with some options', function (done) {
+        let expected = '{"application_id":"app","plan_overrides":{"uri":"uri"},"actions":{}}';
+        let apiClient = new mablApiClient_1.mablApiClient('test');
+        let requestBody = apiClient.buildRequestBody('app', '', '', 'uri', false, false);
+        assert.equal(expected, JSON.stringify(requestBody));
+        done();
+    });
+});
---
+++ deploy-and-test/src/tests/_suite.ts
@@ -0,0 +1,41 @@
+import * as assert from 'assert';
+import {} from 'mocha';
+import {mablApiClient} from '../mablApiClient';
+
+describe('azure pipeline task tests', function() {
+  before(function() {});
+
+  after(() => {});
+
+  it('builds the request correctly with all options', function(done: MochaDone) {
+    let expected: any =
+      '{"environment_id":"env","application_id":"app","plan_overrides":{"browser_types":["firefox"," chrome"," internet_explorer"],"uri":"uri"},"actions":{"rebaseline_images":true,"set_static_baseline":true}}';
+    let apiClient: mablApiClient = new mablApiClient('test');
+    let requestBody = apiClient.buildRequestBody(
+      'app',
+      'env',
+      'firefox, chrome, internet_explorer',
+      'uri',
+      true,
+      true,
+    );
+    assert.equal(expected, JSON.stringify(requestBody));
+    done();
+  });
+
+  it('builds the request correctly with some options', function(done: MochaDone) {
+    let expected: any =
+      '{"application_id":"app","plan_overrides":{"uri":"uri"},"actions":{}}';
+    let apiClient: mablApiClient = new mablApiClient('test');
+    let requestBody = apiClient.buildRequestBody(
+      'app',
+      '',
+      '',
+      'uri',
+      false,
+      false,
+    );
+    assert.equal(expected, JSON.stringify(requestBody));
+    done();
+  });
+});
